d8c73f432332bdc5c9eb222d35136df0813726f2
yang20573 <148195476+yang20573@users.noreply.github.com>
2025-04-01 09:26:53 +0900

    2025/4/1

---
+++ Assets/CameraMover.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Transform targetPosition; 
+    public float moveSpeed = 5f; 
+
+    public void MoveCamera()
+    {
+        StartCoroutine(MoveToTarget());
+    }
+
+    private System.Collections.IEnumerator MoveToTarget()
+    {
+        Transform cameraTransform = Camera.main.transform;
+
+        while (Vector3.Distance(cameraTransform.position, targetPosition.position) > 0.01f)
+        {
+            cameraTransform.position = Vector3.MoveTowards(cameraTransform.position, targetPosition.position, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        cameraTransform.position = targetPosition.position;
+    }
+}
