8ea2d618a6160a16ffe17f124af6e4dfff7e6239
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2025-04-02 21:49:58 +0900

    25.4.2　ミニゲーム更新
    時間システムの追加
    緑素麺の追加
    UI更新

--- Assets/Script/Ri/Soumen.cs
+++ Assets/Script/Ri/Soumen.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Soumen : MonoBehaviour
 {
@@ -18,14 +19,30 @@ public class Soumen : MonoBehaviour
 
     public GameObject soumen_Red;
 
+    public GameObject soumen_Green;
+
     // チェックポイント
     public GameObject checkPoint;
 
     // ポイントText
     public TextMeshProUGUI pointNumber;
 
-    // 赤いボックス生成の確率
-    public int redRandom;
+    // ゲーム時間Text
+    public TextMeshProUGUI gameTimeText;
+    public Image pointImage;
+    public TextMeshProUGUI pointText;
+
+    // 赤と緑ボックス生成の確率
+    public int random_Red;
+    public int random_Green;
+
+
+    // 失敗する場合時間を削る数値
+    public int failedTime = 5;
+
+    // 緑素麺をチェックする場合時間を伸びる数値
+    public int addTime = 5;
+
 
     // 素麺とチェックポイント間の距離。スペースバーを押したとき、
     // 素麺とチェックポイント間の距離はこの値より小さい場合、素麺が消える
@@ -37,32 +54,69 @@ public class Soumen : MonoBehaviour
     // 素麺の移動速度
     public float moveSpeed = 1.0f;
 
+    // 残り時間
+    public float gameTime = 60.0f;
+
     private int point = 0;
 
     private List<GameObject> soumenList_White = new List<GameObject>();
     private List<GameObject> soumenList_Red= new List<GameObject>();
+    private List<GameObject> soumenList_Green = new List<GameObject>();
 
     private float deltaTime = 0.0f;
+    private float deltaTime_Reduce = 0.0f;
+    private bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
         CreateNewSoumen();
-    }
+        pointImage.gameObject.SetActive(false) ;
+}
 
     // Update is called once per frame
     void Update()
     {
-        deltaTime += Time.deltaTime;
-        if (deltaTime >= intervalTime)
+        if (!gameOver)
         {
-            CreateNewSoumen();
-            deltaTime = 0;
-        }
-        MoveSoumen();
+            deltaTime += Time.deltaTime; 
+            deltaTime_Reduce += Time.deltaTime;
+            if (deltaTime_Reduce >= 0.01f)
+            {
+                gameTime -= 0.01f;
+                deltaTime_Reduce = 0;
+            }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            CheckSoumen();
+            if (gameTime<=0.0f)
+            {
+                GameOver();
+            }
+
+            if (gameTime >= 60.0f)
+            {
+                gameTime = 60.0f;
+            }
+            if(gameTime<=0.0f)
+            {
+                gameTime = 0.0f;
+            }
+            if(gameTime<=10.0f)
+            {
+                gameTimeText.color = Color.red;
+            }
+
+            if (deltaTime >= intervalTime)
+            {
+                CreateNewSoumen();
+                deltaTime = 0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                CheckSoumen();
+            }
+
+            MoveSoumen();
+            gameTimeText.text = gameTime.ToString("#0.00");
         }
     }
 
@@ -70,10 +124,15 @@ public class Soumen : MonoBehaviour
     void CreateNewSoumen()
     {
         int random = Random.Range(0, 100);
-        if(random<=redRandom)
+        if(random<= random_Red&&random>random_Green)
         {
             GameObject instance = Instantiate(soumen_Red, startPos.transform.position, startPos.transform.rotation);
             soumenList_Red.Add(instance);
+        }
+        else if(random <= random_Green)
+        {
+            GameObject instance = Instantiate(soumen_Green, startPos.transform.position, startPos.transform.rotation);
+            soumenList_Green.Add(instance);
         }
         else
         {
@@ -96,6 +155,7 @@ public class Soumen : MonoBehaviour
                     {
                         Destroy(soumenList_White[i]);
                         soumenList_White.RemoveAt(i);
+                        gameTime -= failedTime;
                     }
                 }
             }
@@ -111,6 +171,23 @@ public class Soumen : MonoBehaviour
                     {
                         Destroy(soumenList_Red[i]);
                         soumenList_Red.RemoveAt(i);
+                        gameTime -= failedTime;
+                    }
+                }
+            }
+        }
+        if (soumenList_Green != null)
+        {
+            for (int i = 0; i < soumenList_Green.Count; i++)
+            {
+                if (soumenList_Green[i] != null)
+                {
+                    soumenList_Green[i].transform.position = Vector3.MoveTowards(soumenList_Green[i].transform.position, endPos.transform.position, moveSpeed * Time.deltaTime);
+                    if (soumenList_Green[i].transform.position == endPos.transform.position)
+                    {
+                        Destroy(soumenList_Green[i]);
+                        soumenList_Green.RemoveAt(i);
+                        gameTime -= failedTime;
                     }
                 }
             }
@@ -152,5 +229,29 @@ public class Soumen : MonoBehaviour
                 }
             }
         }
+        if (soumenList_Green != null)
+        {
+            for (int i = 0; i < soumenList_Green.Count; i++)
+            {
+                if (soumenList_Green[i] != null)
+                {
+                    if (Vector3.Distance(soumenList_Green[i].transform.position, checkPoint.transform.position) <= checkDistance)
+                    {
+                        gameTime += addTime;
+                        pointNumber.text = point.ToString();
+                        Destroy(soumenList_Green[i]);
+                        soumenList_Green.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+
+    void GameOver()
+    {
+        pointImage.gameObject.SetActive(true);
+        gameOver = true;
+        pointText.text = point.ToString();
+
     }
 }
