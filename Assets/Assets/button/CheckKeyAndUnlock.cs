3e1b58f4677ff02ecb8ea2bde771c33facb09cc0
yang20573 <148195476+yang20573@users.noreply.github.com>
2025-04-18 18:23:50 +0900

    2025/4/18

---
+++ Assets/Assets/button/CheckKeyAndUnlock.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using Fungus;
+using System.Collections.Generic;
+
+public class ItemDialogueHandler : MonoBehaviour
+{
+    public string[] keyNames = { "key" };
+
+    [Tooltip("点击检测到 Key 时发送给 Fungus 的消息名称")]
+    public List<string> Messages1 = new List<string>();
+
+
+    public Flowchart flowchart;
+
+    private void OnMouseDown()
+    {
+        bool foundKey = false;
+        GameObject bag = GameObject.Find("bag");
+        if (bag != null)
+        {
+            foreach (Transform child in bag.transform)
+            {
+                foreach (string keyName in keyNames)
+                {
+                    if (child.name == keyName)
+                    {
+                        Destroy(child.gameObject);
+                        foundKey = true;
+                        break;
+                    }
+                }
+                if (foundKey) break;
+            }
+        }
+            
+
+        if (foundKey)
+        {
+            foreach (string message in Messages1)
+            {
+                flowchart.SendFungusMessage(message);
+                Debug.Log("道具点击检测到 key，发送NoKeyDialogue消息");
+
+            }
+        }
+
+        else
+        {
+            foreach (string message in Messages1)
+            {
+                flowchart.SendFungusMessage(message);
+                Debug.Log("道具点击未检测到 key，发送UnlockedDialogue消息");
+            }
+         
+        }
+    }
+}
