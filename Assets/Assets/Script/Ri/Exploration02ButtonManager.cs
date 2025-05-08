2c5bac27c26aad77a31584d423a9b6f3b5f3d225
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2025-05-08 18:37:27 +0900

    25.5.8
    Exploration02を改善

--- Assets/Assets/Script/Ri/Exploration02ButtonManager.cs
+++ Assets/Assets/Script/Ri/Exploration02ButtonManager.cs
@@ -1,14 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using Fungus;
-using Unity.Burst.CompilerServices;
 using DG.Tweening;
-using UnityEngine.UIElements.Experimental;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Exploration02ButtonManager : MonoBehaviour
 {
@@ -33,24 +25,24 @@ public class Exploration02ButtonManager : MonoBehaviour
     public GameObject ChangeSceneButton_ParentsRoomToCorridor;
 
     [Header("GamePlayButtons_Kyakuma")]
-    public GameObject GamePlayButton_LeftFusuma;
-    public GameObject GamePlayButton_RightFusuma;
+    public GameObject GamePlayButton_Kyakuma_LeftFusuma;
+    public GameObject GamePlayButton_Kyakuma_RightFusuma;
     private int LeftFusumaClickCount = 0;
     private int RightFusumaClickCount = 0;
 
     [Header("GamePlayButtons_Shunou")]
-    public GameObject GamePlayButton_Desk;
-    public GameObject GamePlayButton_Note;
-    public GameObject GamePlayButton_Drawer;
-    public GameObject GamePlayButton_Drawer_Minigame;
-    public GameObject GamePlayButton_Desk_Return;
-    public GameObject GamePlayButton_Note_Return;
-    public GameObject GamePlayButton_Drawer_Return;
-    public GameObject GamePlayButton_Drawer_Minigame_Return;
+    public GameObject GamePlayButton_Shunou_Desk;
+    public GameObject GamePlayButton_Shunou_Note;
+    public GameObject GamePlayButton_Shunou_Drawer;
+    public GameObject GamePlayButton_Shunou_Drawer_Minigame;
+    public GameObject GamePlayButton_Shunou_Desk_Return;
+    public GameObject GamePlayButton_Shunou_Note_Return;
+    public GameObject GamePlayButton_Shunou_Drawer_Return;
+    public GameObject GamePlayButton_Shunou_Drawer_Minigame_Return;
 
     [Header("GamePlayButtons_Corridor")]
-    public GameObject GamePlayButton_GirlRoomPlate;
-    public GameObject GamePlayButton_GirlRoomPlate_Return;
+    public GameObject GamePlayButton_Corridor_GirlRoomPlate;
+    public GameObject GamePlayButton_Corridor_GirlRoomPlate_Return;
 
     [Header("ChangeScene")]
     public GameObject MoveScene_Kyakuma;
@@ -61,15 +53,15 @@ public class Exploration02ButtonManager : MonoBehaviour
 
 
     [Header("GamePlayScene_Shunou")]
-    public GameObject GamePlayScene_Desk;
-    public GameObject GamePlayScene_Note;
-    public GameObject GamePlayScene_Drawer;
-    public GameObject GamePlayScene_Drawer_Minigame;
-    public GameObject GamePlayScene_Drawer_Minigame_UI;
+    public GameObject GamePlayScene_Shunou_Desk;
+    public GameObject GamePlayScene_Shunou_Note;
+    public GameObject GamePlayScene_Shunou_Drawer;
+    public GameObject GamePlayScene_Shunou_Drawer_Minigame;
+    public GameObject GamePlayScene_Shunou_Drawer_Minigame_UI;
 
 
     [Header("GamePlayScene_Corridor")]
-    public GameObject GamePlayScene_GirlRoomPlate;
+    public GameObject GamePlayScene_Corridor_GirlRoomPlate;
 
     [Header("Items_Kyakuma")]
     public GameObject Items_Medal;
@@ -107,23 +99,23 @@ public class Exploration02ButtonManager : MonoBehaviour
         ChangeSceneButton_ParentsRoomToCorridor.GetComponent<Button>().onClick.AddListener(() => OnButtonParentsRoomToCorridorClick());
 
         // Kyakuma Gameplay buttons
-        GamePlayButton_LeftFusuma.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonFusumaClick(true));
-        GamePlayButton_RightFusuma.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonFusumaClick(false));
+        GamePlayButton_Kyakuma_LeftFusuma.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonFusumaClick(true));
+        GamePlayButton_Kyakuma_RightFusuma.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonFusumaClick(false));
 
         // Shunou Gameplay buttons
-        GamePlayButton_Desk.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDeskClick());
-        GamePlayButton_Note.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonNoteClick());
-        GamePlayButton_Drawer.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDrawerClick());
-        GamePlayButton_Drawer_Minigame.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDrawerMinigameClick());
+        GamePlayButton_Shunou_Desk.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDeskClick());
+        GamePlayButton_Shunou_Note.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonNoteClick());
+        GamePlayButton_Shunou_Drawer.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDrawerClick());
+        GamePlayButton_Shunou_Drawer_Minigame.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDrawerMinigameClick());
 
-        GamePlayButton_Desk_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDeskReturnClick());
-        GamePlayButton_Note_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonNoteReturnClick());
-        GamePlayButton_Drawer_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDrawerReturnClick());
-        GamePlayButton_Drawer_Minigame_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDrawerMinigameReturnClick());
+        GamePlayButton_Shunou_Desk_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDeskReturnClick());
+        GamePlayButton_Shunou_Note_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonNoteReturnClick());
+        GamePlayButton_Shunou_Drawer_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDrawerReturnClick());
+        GamePlayButton_Shunou_Drawer_Minigame_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonDrawerMinigameReturnClick());
 
         // Corridor Gameplay buttons
-        GamePlayButton_GirlRoomPlate.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonGirlRoomPlateClick());
-        GamePlayButton_GirlRoomPlate_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonGirlRoomPlateReturnClick());
+        GamePlayButton_Corridor_GirlRoomPlate.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonGirlRoomPlateClick());
+        GamePlayButton_Corridor_GirlRoomPlate_Return.GetComponent<Button>().onClick.AddListener(() => OnGameplayButtonGirlRoomPlateReturnClick());
 
         // Move Scene's Position
         ScenePos_Kyakuma = new Vector3(MoveScene_Kyakuma.GetComponent<Transform>().position.x,
@@ -175,8 +167,8 @@ public class Exploration02ButtonManager : MonoBehaviour
             ChangeSceneButton_KyakumaToDefault.SetActive(true);
             if (!GetMedal)
             {
-                GamePlayButton_LeftFusuma.SetActive(true);
-                GamePlayButton_RightFusuma.SetActive(true);
+                GamePlayButton_Kyakuma_LeftFusuma.SetActive(true);
+                GamePlayButton_Kyakuma_RightFusuma.SetActive(true);
             }
         }); ;
     }
@@ -193,7 +185,7 @@ public class Exploration02ButtonManager : MonoBehaviour
             ChangeSceneButton_CorridorToDefault.SetActive(true);
             ChangeSceneButton_Shunou.SetActive(true);
             ChangeSceneButton_ParentsRoom.SetActive(true);
-            GamePlayButton_GirlRoomPlate.SetActive(true);
+            GamePlayButton_Corridor_GirlRoomPlate.SetActive(true);
         }); ;
 
     }
@@ -202,7 +194,7 @@ public class Exploration02ButtonManager : MonoBehaviour
         ChangeSceneButton_CorridorToDefault.SetActive(false);
         ChangeSceneButton_Shunou.SetActive(false);
         ChangeSceneButton_ParentsRoom.SetActive(false);
-        GamePlayButton_GirlRoomPlate.SetActive(false);
+        GamePlayButton_Corridor_GirlRoomPlate.SetActive(false);
 
         MainCamera.transform.DOMove(ScenePos_Default, MoveSpeed).OnComplete(() =>
         {
@@ -216,33 +208,33 @@ public class Exploration02ButtonManager : MonoBehaviour
         ChangeSceneButton_CorridorToDefault.SetActive(false);
         ChangeSceneButton_Shunou.SetActive(false);
         ChangeSceneButton_ParentsRoom.SetActive(false);
-        GamePlayButton_GirlRoomPlate.SetActive(false);
+        GamePlayButton_Corridor_GirlRoomPlate.SetActive(false);
 
         MainCamera.transform.DOMove(ScenePos_Shunou, MoveSpeed).OnComplete(() =>
         {
             ChangeSceneButton_ShunouToCorridor.SetActive(true);
-            GamePlayButton_Desk.SetActive(true);
+            GamePlayButton_Shunou_Desk.SetActive(true);
         });
     }
     public void OnButtonShunouToCorridorClick()
     {
         ChangeSceneButton_ShunouToCorridor.SetActive(false);
-        GamePlayButton_Desk.SetActive(false);
+        GamePlayButton_Shunou_Desk.SetActive(false);
 
         MainCamera.transform.DOMove(ScenePos_Corridor, MoveSpeed).OnComplete(() =>
         {
             ChangeSceneButton_CorridorToDefault.SetActive(true);
             ChangeSceneButton_Shunou.SetActive(true);
             ChangeSceneButton_ParentsRoom.SetActive(true);
-            GamePlayButton_GirlRoomPlate.SetActive(true);
+            GamePlayButton_Corridor_GirlRoomPlate.SetActive(true);
         });
     }
     // Kyakuma To Default
     public void OnButtonKyakumaToDefaultClick()
     {
         ChangeSceneButton_KyakumaToDefault.SetActive(false);
-        GamePlayButton_LeftFusuma.SetActive(false);
-        GamePlayButton_RightFusuma.SetActive(false);
+        GamePlayButton_Kyakuma_LeftFusuma.SetActive(false);
+        GamePlayButton_Kyakuma_RightFusuma.SetActive(false);
 
         MainCamera.transform.DOMove(ScenePos_Default, MoveSpeed).OnComplete((TweenCallback)(() =>
         {
@@ -255,7 +247,7 @@ public class Exploration02ButtonManager : MonoBehaviour
         ChangeSceneButton_CorridorToDefault.SetActive(false);
         ChangeSceneButton_Shunou.SetActive(false);
         ChangeSceneButton_ParentsRoom.SetActive(false);
-        GamePlayButton_GirlRoomPlate.SetActive(false);
+        GamePlayButton_Corridor_GirlRoomPlate.SetActive(false);
 
         MainCamera.transform.DOMove(ScenePos_ParentsRoom, MoveSpeed).OnComplete((TweenCallback)(() =>
         {
@@ -273,7 +265,7 @@ public class Exploration02ButtonManager : MonoBehaviour
             ChangeSceneButton_CorridorToDefault.SetActive(true);
             ChangeSceneButton_Shunou.SetActive(true);
             ChangeSceneButton_ParentsRoom.SetActive(true);
-            GamePlayButton_GirlRoomPlate.SetActive(true);
+            GamePlayButton_Corridor_GirlRoomPlate.SetActive(true);
         }));
     }
 
@@ -289,7 +281,7 @@ public class Exploration02ButtonManager : MonoBehaviour
             LeftFusumaClickCount += 1;
             if (LeftFusumaClickCount >= 4)
             {
-                GamePlayButton_LeftFusuma.SetActive(false);
+                GamePlayButton_Kyakuma_LeftFusuma.SetActive(false);
                 LeftFusumaClickCount = 4;
             }
         }
@@ -299,7 +291,7 @@ public class Exploration02ButtonManager : MonoBehaviour
             if (RightFusumaClickCount >= 2)
             {
                 RightFusumaClickCount = 2;
-                GamePlayButton_RightFusuma.SetActive(false);
+                GamePlayButton_Kyakuma_RightFusuma.SetActive(false);
             }
         }
 
@@ -313,118 +305,118 @@ public class Exploration02ButtonManager : MonoBehaviour
     // ****************  GamePlayButtons_Corridor ****************
     public void OnGameplayButtonGirlRoomPlateClick()
     {
-        GamePlayScene_GirlRoomPlate.SetActive(true);
-        GamePlayButton_GirlRoomPlate_Return.SetActive(true);
+        GamePlayScene_Corridor_GirlRoomPlate.SetActive(true);
+        GamePlayButton_Corridor_GirlRoomPlate_Return.SetActive(true);
 
-        GamePlayButton_GirlRoomPlate.SetActive(false); 
+        GamePlayButton_Corridor_GirlRoomPlate.SetActive(false); 
         ChangeSceneButton_CorridorToDefault.SetActive(false);
         ChangeSceneButton_Shunou.SetActive(false);
         ChangeSceneButton_ParentsRoom.SetActive(false);
     }
     public void OnGameplayButtonGirlRoomPlateReturnClick()
     {
-        GamePlayButton_GirlRoomPlate.SetActive(true);
+        GamePlayButton_Corridor_GirlRoomPlate.SetActive(true);
         ChangeSceneButton_CorridorToDefault.SetActive(true);
         ChangeSceneButton_Shunou.SetActive(true);
         ChangeSceneButton_ParentsRoom.SetActive(true);
 
-        GamePlayScene_GirlRoomPlate.SetActive(false);
-        GamePlayButton_GirlRoomPlate_Return.SetActive(false);
+        GamePlayScene_Corridor_GirlRoomPlate.SetActive(false);
+        GamePlayButton_Corridor_GirlRoomPlate_Return.SetActive(false);
     }
 
     // ****************  GamePlayButtons_Shunou ****************
     // Desk 
     public void OnGameplayButtonDeskClick()
     {
-        GamePlayButton_Desk.SetActive(false);
+        GamePlayButton_Shunou_Desk.SetActive(false);
         ChangeSceneButton_ShunouToCorridor.SetActive(false);
 
-        GamePlayScene_Desk.SetActive(true);
-        GamePlayButton_Desk_Return.SetActive(true);
+        GamePlayScene_Shunou_Desk.SetActive(true);
+        GamePlayButton_Shunou_Desk_Return.SetActive(true);
 
-        GamePlayButton_Note.SetActive(true);
-        GamePlayButton_Drawer.SetActive(true);
+        GamePlayButton_Shunou_Note.SetActive(true);
+        GamePlayButton_Shunou_Drawer.SetActive(true);
     }
     public void OnGameplayButtonDeskReturnClick()
     {
-        GamePlayButton_Desk.SetActive(true);
+        GamePlayButton_Shunou_Desk.SetActive(true);
         ChangeSceneButton_ShunouToCorridor.SetActive(true);
 
-        GamePlayScene_Desk.SetActive(false);
-        GamePlayButton_Desk_Return.SetActive(false);
-        GamePlayButton_Note.SetActive(false);
-        GamePlayButton_Drawer.SetActive(false);
+        GamePlayScene_Shunou_Desk.SetActive(false);
+        GamePlayButton_Shunou_Desk_Return.SetActive(false);
+        GamePlayButton_Shunou_Note.SetActive(false);
+        GamePlayButton_Shunou_Drawer.SetActive(false);
     }
     // Note
     public void OnGameplayButtonNoteClick()
     {
-        GamePlayScene_Note.SetActive(true);
-        GamePlayButton_Note_Return.SetActive(true);
+        GamePlayScene_Shunou_Note.SetActive(true);
+        GamePlayButton_Shunou_Note_Return.SetActive(true);
 
-        GamePlayButton_Note.SetActive(false);
-        GamePlayButton_Desk_Return.SetActive(false);
+        GamePlayButton_Shunou_Note.SetActive(false);
+        GamePlayButton_Shunou_Desk_Return.SetActive(false);
 
-        GamePlayButton_Drawer.SetActive(false);
+        GamePlayButton_Shunou_Drawer.SetActive(false);
     }
     public void OnGameplayButtonNoteReturnClick()
     {
-        GamePlayButton_Desk_Return.SetActive(true);
-        GamePlayButton_Note.SetActive(true);
-        GamePlayButton_Drawer.SetActive(true);
+        GamePlayButton_Shunou_Desk_Return.SetActive(true);
+        GamePlayButton_Shunou_Note.SetActive(true);
+        GamePlayButton_Shunou_Drawer.SetActive(true);
 
-        GamePlayScene_Note.SetActive(false);
-        GamePlayButton_Note_Return.SetActive(false);
+        GamePlayScene_Shunou_Note.SetActive(false);
+        GamePlayButton_Shunou_Note_Return.SetActive(false);
     }
 
     // Drawer
     public void OnGameplayButtonDrawerClick()
     {
-        GamePlayScene_Drawer.SetActive(true);
-        GamePlayButton_Drawer_Return.SetActive(true);
+        GamePlayScene_Shunou_Drawer.SetActive(true);
+        GamePlayButton_Shunou_Drawer_Return.SetActive(true);
         if (!IsOpenDrawer)
         {
-            GamePlayButton_Drawer_Minigame.SetActive(true);
+            GamePlayButton_Shunou_Drawer_Minigame.SetActive(true);
         }
 
-        GamePlayButton_Drawer.SetActive(false);
-        GamePlayButton_Desk_Return.SetActive(false);
+        GamePlayButton_Shunou_Drawer.SetActive(false);
+        GamePlayButton_Shunou_Desk_Return.SetActive(false);
 
-        GamePlayButton_Note.SetActive(false);
+        GamePlayButton_Shunou_Note.SetActive(false);
     }
     public void OnGameplayButtonDrawerReturnClick()
     {
-        GamePlayButton_Desk_Return.SetActive(true);
-        GamePlayButton_Drawer.SetActive(true);
-        GamePlayButton_Note.SetActive(true);
+        GamePlayButton_Shunou_Desk_Return.SetActive(true);
+        GamePlayButton_Shunou_Drawer.SetActive(true);
+        GamePlayButton_Shunou_Note.SetActive(true);
 
-        GamePlayScene_Drawer.SetActive(false);
-        GamePlayButton_Drawer_Return.SetActive(false);
-        GamePlayButton_Drawer_Minigame.SetActive(false);
+        GamePlayScene_Shunou_Drawer.SetActive(false);
+        GamePlayButton_Shunou_Drawer_Return.SetActive(false);
+        GamePlayButton_Shunou_Drawer_Minigame.SetActive(false);
     }
     public void OnGameplayButtonDrawerMinigameClick()
     {
-        GamePlayScene_Drawer_Minigame.SetActive(true);
-        GamePlayScene_Drawer_Minigame_UI.SetActive(true);
-        GamePlayButton_Drawer_Minigame_Return.SetActive(true);
+        GamePlayScene_Shunou_Drawer_Minigame.SetActive(true);
+        GamePlayScene_Shunou_Drawer_Minigame_UI.SetActive(true);
+        GamePlayButton_Shunou_Drawer_Minigame_Return.SetActive(true);
 
-        GamePlayButton_Drawer_Minigame.SetActive(false);
-        GamePlayButton_Drawer_Return.SetActive(false);
+        GamePlayButton_Shunou_Drawer_Minigame.SetActive(false);
+        GamePlayButton_Shunou_Drawer_Return.SetActive(false);
     }
     public void OnGameplayButtonDrawerMinigameReturnClick()
     {
-        GamePlayScene_Drawer_Minigame.SetActive(false);
-        GamePlayScene_Drawer_Minigame_UI.SetActive(false);
-        GamePlayButton_Drawer_Minigame_Return.SetActive(false);
+        GamePlayScene_Shunou_Drawer_Minigame.SetActive(false);
+        GamePlayScene_Shunou_Drawer_Minigame_UI.SetActive(false);
+        GamePlayButton_Shunou_Drawer_Minigame_Return.SetActive(false);
 
         if (!IsOpenDrawer)
         {
-            GamePlayButton_Drawer_Minigame.SetActive(true);
+            GamePlayButton_Shunou_Drawer_Minigame.SetActive(true);
         }
-        GamePlayButton_Drawer_Return.SetActive(true);
+        GamePlayButton_Shunou_Drawer_Return.SetActive(true);
     }
     public void OpenDrawer()
     {
-        GamePlayScene_Drawer.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration02_2F_Shunou_drawer_open");
+        GamePlayScene_Shunou_Drawer.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration02_2F_Shunou_drawer_open");
         IsOpenDrawer = true;
     }
 
