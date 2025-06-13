3ed8a9d0fd88065c614c12358dca6a36ada73407
ZhusSong <42826951+ZhusSong@users.noreply.github.com>
2025-06-13 18:45:23 +0900

    25.6.13
    add Exploration03
    change file directory

--- Assets/Assets/Script/Ri/Exploration02ButtonManager.cs
+++ Assets/Assets/Script/Ri/Exploration02ButtonManager.cs
@@ -634,7 +634,7 @@ public class Exploration02ButtonManager : MonoBehaviour
         if (IsOpenLeftFusuma && IsOpenRightFusuma)
         {
             Exploration_AudioManager.PlaySE_External(Exploration02_SE[1], 0.8f);
-            MoveScene_Kyakuma.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration02_Kyakuma_Door_Open");
+            MoveScene_Kyakuma.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration_02/Exploration02_Kyakuma_Door_Open");
          
             Items_Kyakuma_Medal.SetActive(true);
             GamePlayButton_Kyakuma_Medal.SetActive(true);
@@ -658,7 +658,7 @@ public class Exploration02ButtonManager : MonoBehaviour
         if(IsGetBrushMedal&&IsGetDentou&&IsGetHornMedal&&IsGetOnpuMedal)
         {
             Exploration_AudioManager.PlaySE_External(Exploration02_SE[1], 0.8f);
-            GamePlayScene_Corridor_GirlRoomPlate.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration02_2F_GirlRoom_plate_Ver2");
+            GamePlayScene_Corridor_GirlRoomPlate.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration_02/Exploration02_2F_GirlRoom_plate_Ver2");
             GamePlayButton_Corridor_GoToNextScene.SetActive(true);
         }
 
@@ -804,7 +804,7 @@ public class Exploration02ButtonManager : MonoBehaviour
     public void OpenDrawer()
     {
         Exploration_AudioManager.PlaySE_External(Exploration02_SE[1], 0.8f);
-        GamePlayScene_Shunou_Drawer.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration02_2F_Shunou_drawer_open");
+        GamePlayScene_Shunou_Drawer.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration_02/Exploration02_2F_Shunou_drawer_open");
         GamePlayScene_Shunou_Drawer_Minigame.SetActive(false);
         GamePlayButton_Shunou_Drawer_Key.SetActive(true);
         Items_Shunou_Key.SetActive(true);
@@ -848,7 +848,7 @@ public class Exploration02ButtonManager : MonoBehaviour
     public void OpenCloset()
     {
         Exploration_AudioManager.PlaySE_External(Exploration02_SE[1], 0.8f);
-        GamePlayScene_ParentsRoom_Closet.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploraton02_ParentsRoom_ClosetOpen");
+        GamePlayScene_ParentsRoom_Closet.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration_02/Exploraton02_ParentsRoom_ClosetOpen");
         GamePlayButton_ParentsRoom_OnpuMedal.SetActive(true);
         Items_ParentsRoom_OnpuMedal.SetActive(true);
         IsOpenCloset = true;
@@ -971,7 +971,7 @@ public class Exploration02ButtonManager : MonoBehaviour
     public void OpenClock()
     {
         Exploration_AudioManager.PlaySE_External(Exploration02_SE[1], 0.8f);
-        GamePlayScene_Libing_ClockMinigame.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration02_Libing_Openclock");
+        GamePlayScene_Libing_ClockMinigame.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration_02/Exploration02_Libing_Openclock");
         GamePlayButton_Libing_BrushMedal.SetActive(true);
         Items_Libing_BrushMedal.SetActive(true);
         IsOpenClock = true;
@@ -1024,7 +1024,7 @@ public class Exploration02ButtonManager : MonoBehaviour
         Exploration_AudioManager.PlaySE_External(Exploration02_SE[4], 0.8f);
         DishIsClean = true;
         GamePlayButton_Kitchen_CleanDish.SetActive(false);
-        GamePlayScene_Kithchen_Dish.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration02_Item_Dish");
+        GamePlayScene_Kithchen_Dish.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RI/Exploration_02/Exploration02_Item_Dish");
     }
     public void OnGameplayButtonDishReturnClick()
     {
