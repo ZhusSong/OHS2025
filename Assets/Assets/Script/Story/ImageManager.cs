605b6d46d5b7042b73ef2796552ce5917bded711
Hshhsji <taiyou.912@icloud.com>
2025-06-13 18:39:02 +0900

    Story2-2 途中まで実装

--- Assets/Assets/Script/Story/ImageManager.cs
+++ Assets/Assets/Script/Story/ImageManager.cs
@@ -43,6 +43,7 @@ public class ImageManager : MonoBehaviour
     public void ShowObject24() { ShowObjectByIndex(23); }
     public void ShowObject25() { ShowObjectByIndex(24); }
     public void ShowObject26() { ShowObjectByIndex(25); }
+    public void ShowObject27() { ShowObjectByIndex(26); }
 
     private void ShowObjectByIndex(int index)
     {
